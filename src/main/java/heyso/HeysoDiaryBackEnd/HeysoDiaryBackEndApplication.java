b16ad9bb3354fc331127c867d2e6bc4a8650399e
soyoung <makemeha2@gmail.com>
2025-11-12 18:39:31 +0900

    first commit

---
+++ src/main/java/heyso/HeysoDiaryBackEnd/HeysoDiaryBackEndApplication.java
@@ -0,0 +1,12 @@
+package heyso.HeysoDiaryBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeysoDiaryBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeysoDiaryBackEndApplication.class, args);
+	}
+}
---
+++ src/test/java/heyso/HeysoDiaryBackEnd/HeysoDiaryBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package heyso.HeysoDiaryBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeysoDiaryBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
