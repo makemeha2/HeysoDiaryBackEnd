43e5499a394cebecd4ca30f4d2ba2f63bd8609b6
soyoung <makemeha2@gmail.com>
2025-12-15 18:41:15 +0900

    CORS 오류 수정

--- src/main/java/heyso/HeysoDiaryBackEnd/config/CorsConfig.java
+++ src/main/java/heyso/HeysoDiaryBackEnd/config/CorsConfig.java
@@ -1,22 +1,24 @@
 package heyso.HeysoDiaryBackEnd.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class CorsConfig {
 
     @Bean
-    public CorsFilter corsFilter() {
+    public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        // config.addAllowedOriginPattern("*");
-        config.addAllowedOrigin("http://localhost:5173");
-        config.addAllowedOrigin("http://127.0.0.1");
+        config.setAllowedOrigins(List.of(
+                        "http://localhost:5173",
+                        "http://127.0.0.1:5173"));
 
         config.addAllowedHeader("*");
         config.addAllowedMethod("*"); // GET, POST, PUT, DELETE 등 모든 메소드 허용
@@ -24,6 +26,6 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
 
-        return new CorsFilter(source);
+        return source;
     }
 }
--- src/main/java/heyso/HeysoDiaryBackEnd/config/SecurityConfig.java
+++ src/main/java/heyso/HeysoDiaryBackEnd/config/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
         public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
                 http
+                                .cors(Customizer.withDefaults())
                                 .csrf(csrf -> csrf.disable())
                                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
