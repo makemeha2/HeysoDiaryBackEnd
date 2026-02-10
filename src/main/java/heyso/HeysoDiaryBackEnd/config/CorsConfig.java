b2c2737e543d4835e5c3ecfdb64b55ad51557b2a
soyoung <makemeha2@gmail.com>
2026-02-10 19:13:24 +0900

    배포관련 코드 수정

--- src/main/java/heyso/HeysoDiaryBackEnd/config/CorsConfig.java
+++ src/main/java/heyso/HeysoDiaryBackEnd/config/CorsConfig.java
@@ -17,8 +17,10 @@ public class CorsConfig {
 
         config.setAllowCredentials(true);
         config.setAllowedOrigins(List.of(
-                        "http://localhost:5173",
-                        "http://127.0.0.1:5173"));
+                "http://localhost:5173",
+                "http://127.0.0.1:5173",
+                "http://localhost:8080",
+                "http://127.0.0.1:8080"));
 
         config.addAllowedHeader("*");
         config.addAllowedMethod("*"); // GET, POST, PUT, DELETE 등 모든 메소드 허용
