cb4b6eac7783f83a794a41161eaa184574fd5670
makemeha2 <makemeha2@gmail.com>
2026-02-17 23:41:32 +0900

    CORS 정책 추가

--- src/main/java/heyso/HeysoDiaryBackEnd/config/CorsConfig.java
+++ src/main/java/heyso/HeysoDiaryBackEnd/config/CorsConfig.java
@@ -20,7 +20,8 @@ public class CorsConfig {
                 "http://localhost:5173",
                 "http://127.0.0.1:5173",
                 "http://localhost:8080",
-                "http://127.0.0.1:8080"));
+                "http://127.0.0.1:8080",
+                "https://heyso-diary.com"));
 
         config.addAllowedHeader("*");
         config.addAllowedMethod("*"); // GET, POST, PUT, DELETE 등 모든 메소드 허용
